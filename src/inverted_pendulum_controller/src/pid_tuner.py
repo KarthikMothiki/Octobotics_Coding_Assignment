c8729e47024a2d1b307532350b28dbb5422e21e1
Karthik Mothiki <karthik1111mothiki@gmail.com>
2023-12-31 14:32:49 +0530

    Update pid_tuner.py

--- src/inverted_pendulum_controller/src/pid_tuner.py
+++ src/inverted_pendulum_controller/src/pid_tuner.py
@@ -1,52 +1,39 @@
 #!/usr/bin/env python
 
 import rospy
-from math import pi
-from inverted_pendulum_sim.msg import ControlForce
-from inverted_pendulum_sim.msg import CurrentState
-from std_msgs.msg import Float64
+from inverted_pendulum_sim.msg import CurrentState, ControlForce
+from simple_pid import PID
 
-class PIDTuner:
+class InvertedPendulumController:
     def __init__(self):
-        rospy.init_node("pid_tuner", anonymous=True)
-
-        # PID controller parameters
-        self.Kp = 1.0
-        self.Ki = 0.5
-        self.Kd = 0.01
-
-        # State variables
-        self.prev_error = 0.0
-        self.integral = 0.0
-        print(self.integral)
-
-        # Set up publishers and subscribers
+        rospy.init_node("inverted_pendulum_controller", anonymous=True)
         self.control_force_pub = rospy.Publisher('/inverted_pendulum/control_force', ControlForce, queue_size=10)
-        self.theta_sub = rospy.Subscriber('/inverted_pendulum/current_state', CurrentState, self.theta_callback)
+        self.current_state_sub = rospy.Subscriber('/inverted_pendulum/current_state', CurrentState, self.state_callback)
+        
+        # Initialize PID controllers for theta and x
+        self.pid_theta = PID(3.95, 3.1, 0.01, setpoint=0)  # Tune these gains as needed
+        self.pid_x = PID(4.3, 1.3, 0.01, setpoint=0)  # Tune these gains as needed
 
-    def theta_callback(self, msg):
-        # Desired angle for the pendulum to be balanced (inverted)
-        desired_theta = pi
+    def state_callback(self, msg):
+        theta_feedback = msg.curr_theta
+        x_feedback = msg.curr_x
 
-        # Calculate error terms
-        error = desired_theta - msg.curr_theta
-        self.integral += error * 0.1 # Adjust the integration time
+        # Update setpoints for the PID controllers (desired theta and x)
+        self.pid_theta.setpoint = 0  # Set desired theta
+        self.pid_x.setpoint = 0  # Set desired x
 
-        # Calculate control force using PID formula
-        control_force = self.Kp * error + self.Ki * self.integral + self.Kd * (error - self.prev_error) / 0.1
+        # Compute PID outputs
+        control_force_theta = self.pid_theta(theta_feedback)
+        control_force_x = self.pid_x(x_feedback)
 
-        # Apply the control force to the pendulum
+        # Combine or manipulate these control forces as needed for your specific control strategy
+        total_control_force = control_force_theta + control_force_x  # Modify this according to your requirements
+
+        # Publish the combined control force
         control_msg = ControlForce()
-        control_msg.force = control_force
+        control_msg.force = total_control_force
         self.control_force_pub.publish(control_msg)
 
-        # Update previous error for the next iteration
-        self.prev_error = error
-        print(self.prev_error )
-
 if __name__ == '__main__':
-    pid_tuner = PIDTuner()
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        rospy.loginfo("[PIDTuner]: Shutting down node")
+    pendulum_controller = InvertedPendulumController()
+    rospy.spin()
