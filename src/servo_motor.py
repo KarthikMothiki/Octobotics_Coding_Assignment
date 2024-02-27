b32abbb92be1051e000b7f4f9f22d9094f5b7958
Karthik Mothiki <62557178+KarthikMothiki@users.noreply.github.com>
2024-02-27 22:04:13 +0530

    Update servo_motor.py

--- src/servo_motor.py
+++ src/servo_motor.py
@@ -1,74 +1,81 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import Jetson.GPIO as GPIO
 import time
 import rospy
 from std_srvs.srv import Trigger, TriggerResponse
 
-servo_pin = 15
-trigger_pin = 7  # GPIO pin to trigger PWM signal
-GPIO.setwarnings(False)
+servo_pin = 18
+gpio_pin = 7
+p = None  # Global variable to hold PWM object
 
-p = None  # Declare p globally
+def move_motor(angle):
 
-def servo_move(angle):
-    print("Came in to the servo_move func")
+    p.start(0)
     duty_0_deg = 2.5
     duty_180_deg = 12.5
     duty_cycle = ((angle /180.0) * (duty_180_deg - duty_0_deg)) + duty_0_deg
     p.ChangeDutyCycle(duty_cycle)
+
     print('Moved to', angle, ' deg')
-    time.sleep(1)          
+    time.sleep(1)
+         
     for angle in range(angle, 0, -1):
-        duty_cycle = angle / angle + 2.0
+        duty_cycle = angle / 18.0 + 2.0
         p.ChangeDutyCycle(duty_cycle)
         time.sleep(0.05) 
     print('Moved to', 0, ' deg')
 
+    mes = "PWM signal sent"
+    return mes
+
 def pwm_control_callback(req):
-    val = 8
-    print("Came in to the callback func")
-    print("PWM running. Press CTRL+C to exit.")
-    try:
-        if GPIO.input(trigger_pin) == GPIO.HIGH or rospy.get_param('/servo_control_1', True):
-            servo_move(val)
-            print("Executed servo_move function")
-        else: 
-            print("No signal received from Pin 7 or Service Call")
-    except KeyboardInterrupt:
-        pass
+    # Your PWM control logic here
+    # For example, set the servo to a specific angle
+    # Adjust val or any other parameters based on req data
+    val = 90
+    mes = move_motor(val)
+
     # Return success message
-    return TriggerResponse(success=True, message="PWM signal sent")
+    return TriggerResponse(success=True, message = mes)
 
 def main():
-    global p
-
-    # Pin Setup:
-    # Board pin-numbering scheme
-    GPIO.setmode(GPIO.BOARD)
-
-    # set pin as an output pin with optional initial state of HIGH
-    GPIO.setup(trigger_pin, GPIO.IN)
-    GPIO.setup(servo_pin, GPIO.OUT)
-
-    p = GPIO.PWM(servo_pin, 50)
-    p.start(0)
-
-    # Initialize ROS node
-    rospy.init_node('servo_control_1', anonymous=True)
-
-    # Create ROS service
-    rospy.Service('/servo_control_1', Trigger, pwm_control_callback)
-
+    global p  # Access the global PWM object
     try:
-        rospy.spin()  # Enter ROS event loop
+        # Pin Setup:
+        # Board pin-numbering scheme
+        GPIO.setmode(GPIO.BOARD)
+        # Set servo pin as an output pin with optional initial state of HIGH
+        GPIO.setup(servo_pin, GPIO.OUT)
+        GPIO.setup(gpio_pin, GPIO.IN)
+        p = GPIO.PWM(servo_pin, 50)
+
+        # Set GPIO pin 7 as input
+        GPIO.setup(gpio_pin, GPIO.IN)
+
+        # Initialize ROS node
+        rospy.init_node('servo_control')
+
+        # Create ROS service
+        rospy.Service('/servo_control', Trigger, pwm_control_callback)
+
+        print ('PWM running. Press CTRL+C to exit.')
+        while not rospy.is_shutdown():
+            time.sleep(0.25)
+
+            # Check if GPIO pin 22 is high or service is called
+            if GPIO.input(gpio_pin) == GPIO.HIGH or rospy.get_param('/servo_control', False):
+
+                # Send PWM signal
+                pwm_control_callback(None)
+
+                
     except KeyboardInterrupt:
         pass
     finally:
-        # Stop PWM and cleanup GPIO pins
         if p is not None:
             p.stop()
-        GPIO.cleanup()
+            GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
